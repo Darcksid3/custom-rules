2f9f716f00de9ec8175b8676e09af537680107c3
celine deshayes <deshayesceline@outlook.fr>
2026-05-27 11:33:56 +0200

    show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,10 @@
 class GamesController < ApplicationController
-IMAGES_GAME = {
-  "monopoly" => "https://cdn.cultura.com/cdn-cgi/image/width=1280/media/pim/23_244147_1_10_FR.jpg",
-  "skyjo" => "https://cdn.cultura.com/cdn-cgi/image/width=1280/media/pim/3017472_1njprxew_1",
-  "uno" => "https://m.media-amazon.com/images/I/71n61AtC+VL._AC_SL1500_.jpg",
-  "scrabble" => "https://cdn.cultura.com/cdn-cgi/image/width=1280/media/pim/77_241905_1_10_FR.jpg"
-}
+  IMAGES_GAME = {
+    "monopoly" => "https://cdn.cultura.com/cdn-cgi/image/width=1280/media/pim/23_244147_1_10_FR.jpg",
+    "skyjo" => "https://cdn.cultura.com/cdn-cgi/image/width=1280/media/pim/3017472_1njprxew_1",
+    "uno" => "https://m.media-amazon.com/images/I/71n61AtC+VL._AC_SL1500_.jpg",
+    "scrabble" => "https://cdn.cultura.com/cdn-cgi/image/width=1280/media/pim/77_241905_1_10_FR.jpg"
+  }
 
   def index
     @games = Game.all
