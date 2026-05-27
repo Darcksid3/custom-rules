1b810c8c6122204a064568ece6b7e0a63a0d0eb3
bruno rojo <mr.rojo.bruno@gmail.com>
2026-05-28 00:18:01 +0200

    mise en place de la conversation et ajout des gem pour le markdown

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,8 @@
 class MessagesController < ApplicationController
 
+  SYSTEM_PROMPT = "Tu est un expert en comception de règle de jeux de société.\n\n
+                   ton but est d'aider l'utilisateur à crée des règles de jeux personalisez.\n\n
+                   en fonction de ces instructions. répond toujours en Markdown."
   def show
     # on affiche tout les mesaage de la conversation
   end
@@ -9,5 +12,35 @@ class MessagesController < ApplicationController
 
   def create
     # on crée le message et on l'envoie en base de donnée 
+    @conversation = current_user.conversations.find(params[:conversation_id])
+    @game = @conversation.game
+
+    @message = Message.new(message_params)
+    @message.conversation = @conversation
+    @message.role = "user"
+
+    if @message.save
+      ruby_llm_chat = RubyLLM.chat
+      response = ruby_llm_chat.with_instructions(instructions).ask(@message.content)
+      Message.create(role: "assistant", content: response.content, conversation: @conversation)
+      @conversation.generate_title_from_first_message # NEW
+      redirect_to conversation_path(@conversation)
+    else
+      render "conversation/show", status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def game_context
+  "Voici le jeux: #{@game.name}."
+end
+
+def instructions
+  [SYSTEM_PROMPT, game_context].compact.join("\n\n")
+end
 end
